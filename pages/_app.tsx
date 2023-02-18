0e95b99689ad2909952226beeb619822d858b327
Arkhip Podobed <carielis.easy@gmail.com>
2023-02-19 00:47:09 +0400

    add title for page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { Wrapper } from "../src/components/pages/MainContent/MainContent";
-
+import Head from "next/head";
+import favico from "../public/images/favicon.ico";
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const isWrapper = router.asPath !== "/" && router.asPath !== "/select-hero";
@@ -15,6 +16,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         color: "black",
       }}
     >
+      <Head>
+        <title>Росмолодежь</title>
+        <link
+          rel="shortcut icon"
+          href={"/public/images/favicon.ico"}
+          type="image/x-icon"
+        />
+      </Head>
       {isWrapper ? (
         <Wrapper>
           <Component {...pageProps} />
