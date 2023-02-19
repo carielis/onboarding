1b865c806b86de206471696e75ed11ebeb45e0e9
Arkhip Podobed <carielis.easy@gmail.com>
2023-02-19 04:53:43 +0400

    fix buiild error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,6 +22,11 @@ function MyApp({ Component, pageProps }: AppProps) {
     >
       <Head>
         <title>Росмолодежь</title>
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap')`,
+          }}
+        ></style>
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <link
           rel="apple-touch-icon"
